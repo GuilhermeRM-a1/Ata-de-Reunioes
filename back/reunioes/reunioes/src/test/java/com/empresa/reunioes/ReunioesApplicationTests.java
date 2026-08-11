552883b9f7276801c856304921dfe1b1b167fd3d
Guilherme R.M <emaildoguilherme16@gmail.com>
2026-08-10 21:33:49 -0300

    iniciando o projeto

---
+++ back/reunioes/reunioes/src/test/java/com/empresa/reunioes/ReunioesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.reunioes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunioesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
