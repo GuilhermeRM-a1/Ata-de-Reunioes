45c00244b3fa054b5b570442553a4b7a6f3f98d1
Guilherme R.M <emaildoguilherme16@gmail.com>
2026-09-03 20:18:46 -0300

    feat: mapeamento jpa tabelas e colunas sem relacionamento

--- back-reunioes/src/main/java/br/com/empresa/reunioes/domain/entity/Colaborador.java
+++ back-reunioes/src/main/java/br/com/empresa/reunioes/domain/entity/Colaborador.java
@@ -9,20 +9,31 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@Table(name = "")
 public class Colaborador {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "")
     private String nome;
+
+    @Column(name = "")
     private String senha;
+
+    @Column(name = "")
     private Boolean monitorarReunioes;
+
+    @Column(name = "")
     private String dataCadastro;
+
+    @Column(name = "")
     private Boolean admin;
 
     @OneToMany
     private List<Acao> acoes;
+
 
     @ManyToMany(mappedBy = "participantes")
     private List<Reuniao> reunioes;
