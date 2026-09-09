4263a29d31d1a7227bd84035b5a0ab15b0d99953
Guilherme R.M <emaildoguilherme16@gmail.com>
2026-09-08 21:50:31 -0300

    feat: relacionamentos jpa

--- back-reunioes/src/main/java/br/com/empresa/reunioes/domain/entity/Acao.java
+++ back-reunioes/src/main/java/br/com/empresa/reunioes/domain/entity/Acao.java
@@ -9,26 +9,29 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@Table(name = "")
+@Table(name = "acao")
 public class Acao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "")
+    @Column(name = "titulo")
     private String titulo;
 
-    @Column(name = "")
+    @Column(name = "descricao")
     private String descricao;
 
-    @Column(name = "")
+    @Column(name = "tipo")
     private String tipo;
 
-    @Column(name = "")
+    @Column(name = "prazo")
     private String prazo;
 
     @ManyToMany
+    @JoinTable(name = "acao_responsavel",
+            joinColumns = @JoinColumn(name = "acao_id"),
+            inverseJoinColumns = @JoinColumn(name = "responsavel_id"))
     private List<Colaborador> responsavel;
 
     @ManyToOne
--- back-reunioes/src/main/java/br/com/empresa/reunioes/domain/entity/Reuniao.java
+++ back-reunioes/src/main/java/br/com/empresa/reunioes/domain/entity/Reuniao.java
@@ -9,37 +9,45 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@Table(name = "")
+@Table(name = "reuniao")
 public class Reuniao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "")
+    @Column(name = "titulo")
     private String titulo;
 
-    @Column(name = "")
+    @Column(name = "data")
     private String data;
 
-    @Column(name = "")
+    @Column(name = "resumo")
     private String resumo;
 
-    @Column(name = "")
+    @Column(name = "status")
     private String status;
 
     @ManyToMany
+    @JoinTable(name = "reuniao_participantes",
+            joinColumns = @JoinColumn(name = "reuniao_id"),
+            inverseJoinColumns = @JoinColumn(name = "participantes_id"))
     private List<Colaborador> participantes;
 
     @ElementCollection
+    @CollectionTable(name = "reuniao_areas", joinColumns = @JoinColumn(name = "reuniao_id"))
+    @Column(name = "areas")
     private List<String> areas;
 
     @ElementCollection
+    @CollectionTable(name = "reuniao_pontos_chaves", joinColumns = @JoinColumn(name = "reuniao_id"))
+    @Column(name = "pontos_chaves")
     private List<String> pontosChaves;
 
-    @OneToMany
+    @OneToMany(mappedBy = "reuniao")
     private List<Acao> acoes;
 
+    @Column(name = "total_acoes")
     private Integer totalAcoes;
 
 }
