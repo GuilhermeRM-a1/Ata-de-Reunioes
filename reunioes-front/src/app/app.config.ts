9b5a6205656b703d28ef2aeb49a2e703c416d859
Guilherme R.M <emaildoguilherme16@gmail.com>
2026-09-11 21:05:09 -0300

    retirando redundancia

--- reunioes-front/src/app/app.config.ts
+++ reunioes-front/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
